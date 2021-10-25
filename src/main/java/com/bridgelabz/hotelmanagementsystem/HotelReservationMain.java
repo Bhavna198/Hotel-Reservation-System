657aae87852e9c7d6b675b93d29a31098ac5b11b
Bhavna Shende <bhavanashende661@gmail.com>
2021-10-25 20:30:56 +0530

    Program of Hotel Reservation System

---
+++ src/main/java/com/bridgelabz/hotelmanagementsystem/HotelReservationMain.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.hotelmanagementsystem;
+
+public class HotelReservationMain {
+    public static void main(String[] args) {
+        System.out.println("............Hotel Reservation System...........");
+    }
+}
